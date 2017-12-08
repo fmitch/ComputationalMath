6abe3954a2328f2ce470e3662be36f572b80a7fd
Frost Mitchell <Frost@server.greenjacket.private>
2017-12-08 13:11:30 -0700

    new main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -1,17 +1,34 @@
-#include "src/error/precision.hpp"
-#include "src/approximations/series.hpp"
+#include "src/linalg/norms.hpp"
+#include "src/linalg/matop.hpp"
+#include "src/linalg/parmatop.hpp"
+#include "src/linalg/print.hpp"
+#include "src/linalg/error.hpp"
+#include "src/matrix_solutions/iterative.hpp"
+#include "src/eigenvalues/power.hpp"
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <limits.h>
 
 int main(){
-    int pres = singleMacEps();
-    printf("precision of float is %d digits.\n", pres); 
-    pres = doubleMacEps();
-    printf("precision of double is %d digits.\n", pres); 
-    pres = longDoubleMacEps();
-    printf("precision of long double is %d digits\n", pres); 
-
-    double ex1 = expSeries(1);
-    printf("e is %f \n", ex1); 
+    double A[32][32];
+    double guess[32];
+    for (int i = 0; i < 32; i++)
+    {
+        A[i][i] = i+1;
+        guess[i] = (double)rand()/INT_MAX;
+    }
+    printVec(guess, 32);
+    double lambda = powerMethod((double*) A, guess, 32, 1e-8, 500);
+    A[30][30] = 30;
+    printf("A DONE\n \n \n");
+    lambda = powerMethod((double*) A, guess, 32, 1e-8, 500);
+    printf("B DONE\n \n \n");
+/*
+    double A[][4] = {{5, .1,.2,.3},{.2,6,.1,.4},{.8,.6,8.7,1},{.3,.1,.9,10}};
+    double b[] = {1,2,3,4};
+    double lambda = powerMethod((double*) A, b, 4, 1e-8, 500);
+    
+    */
     return 0;
-
 }
